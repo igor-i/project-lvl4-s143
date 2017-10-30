d612bf015a2baeeb1338f654f7609d73afe3921b
Igor Inkovskiy <Inkovskiy@gmail.com>
2017-10-30 14:21:44 +0300

    test(Users) disable tests

--- tests/Feature/UsersTest.php
+++ tests/Feature/UsersTest.php
@@ -6,32 +6,32 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Artisan;
 
-class DomainsTest extends TestCase
+class UsersTest extends TestCase
 {
     public function setUp()
     {
         parent::setUp();
-        Artisan::call('migrate');
+//        Artisan::call('migrate');
     }
 
     public function tearDown()
     {
-        Artisan::call('migrate:reset');
+//        Artisan::call('migrate:reset');
         parent::tearDown();
     }
 
-    public function testForm()
-    {
+//    public function testLogin()
+//    {
 //        $this->post('/domains', ['url' => 'http://ya.ru']);
 //        Artisan::call('queue:work', ['--once' => true]);
 //        $this->seeInDatabase('domains', ['name' => 'http://ya.ru']);
-    }
+//    }
 
-    public function testApplication()
-    {
-        $this->post('/domains', ['url' => 'http://ya.ru']);
-        Artisan::call('queue:work', ['--once' => true]);
-        $this->get('/domains');
-        $this->assertResponseOk();
-    }
+//    public function testApplication()
+//    {
+//        $this->post('/domains', ['url' => 'http://ya.ru']);
+//        Artisan::call('queue:work', ['--once' => true]);
+//        $this->get('/domains');
+//        $this->assertResponseOk();
+//    }
 }
