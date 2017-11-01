2bc76d9254c7d2ce45b2e97563c068248687ef2e
Igor Inkovskiy <Inkovskiy@gmail.com>
2017-11-02 01:20:48 +0300

    test(Login)

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -12,8 +12,8 @@ use App\User;
 
 class LoginTest extends TestCase
 {
-//    use DatabaseMigrations;
-    use RefreshDatabase;
+    use DatabaseMigrations;
+//    use RefreshDatabase;
 
     /**
      * @return void
@@ -38,7 +38,7 @@ class LoginTest extends TestCase
             'password' => '111111'
         ]);
 
-        $this->assertTrue(Auth::user()->name == 'Test');
+        $this->assertTrue($response->Auth::user()->name == 'Test');
 //        $response->assertViewIs('task');
     }
 }
