eaa7858d1a402f21a90f43cfdc632c790a79558d
Igor Inkovskiy <Inkovskiy@gmail.com>
2017-11-02 02:04:36 +0300

    test(Login)

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -5,8 +5,6 @@ namespace Tests\Feature;
 use Tests\TestCase;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Http\Request;
 
 use App\User;
 
@@ -23,26 +21,6 @@ class LoginTest extends TestCase
         $response->assertStatus(200);
     }
 
-    public function testAuthenticate()
-    {
-        factory(User::class)->create([
-            'name' => 'Test',
-            'email' => 'test@test.io',
-            'password' => '111111'
-        ]);
-
-        if (Auth::attempt([
-            'name' => 'Test',
-            'email' => 'test@test.io',
-            'password' => '111111'
-        ])) {
-            // Authentication passed...
-            $this->assertTrue(true);
-        } else {
-            $this->assertTrue(false);
-        }
-    }
-
     public function testLoginForm()
     {
         factory(User::class)->create([
@@ -57,31 +35,6 @@ class LoginTest extends TestCase
             'password' => '111111'
         ]);
 
-        if ($response->Auth::check()) {
-            $this->assertTrue(true);
-        } else {
-            $this->assertTrue(false);
-        }
-
-//        $response->assertTrue($request->Auth::user()->name == 'Test');
-//        $response->assertViewIs('task');
-    }
-
-    public function testLoginForm2()
-    {
-        factory(User::class)->create([
-            'name' => 'Test2',
-            'email' => 'test2@test.io',
-            'password' => '111111'
-        ]);
-
-        $response = $this->post('/login', [
-            'name' => 'Test2',
-            'email' => 'test2@test.io',
-            'password' => '111111'
-        ]);
-
-//        $response->assertTrue($response->user()->name == 'Test');
         $response->assertViewIs('task');
     }
 }
