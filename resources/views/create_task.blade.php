3b856b707702c60725b9c4f1bd6c8cf9d5952a05
Igor Inkovskiy <Inkovskiy@gmail.com>
2017-11-07 22:41:09 +0300

    fix(Tags) error in heroku

---
+++ resources/views/create_task.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('title', 'Create tag')
+
+@section('content')
+    <div class="card" style="width: 30rem;">
+        <div class="card-body">
+            <h4 class="card-title">Create tag</h4>
+            <h6 class="card-subtitle mb-2 text-muted">Create a new tag</h6>
+            <p class="card-text">
+            <form method="POST" action="{{ route('tags.store') }}">
+                {{ csrf_field() }}
+
+                <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                    <label for="name">Name</label>
+                    <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}" required autofocus>
+
+                    @if ($errors->has('name'))
+                        <span class="help-block text-danger">
+                            <strong>{{ $errors->first('name') }}</strong>
+                        </span>
+                    @endif
+                </div>
+
+                <div class="form-group">
+                    <input type="submit" class="btn btn-primary" value="Create" data-disable-with="Creating...">
+                    <a type="button" class="btn btn-light" role="button" href="{{ route('tags.index') }}">
+                        Cancel
+                    </a>
+                </div>
+            </form>
+            </p>
+        </div>
+    </div>
+@endsection
